0b3e597e918c7740014c9fe036f414e35dbf6a4e
HarshRajput2 <rajput29harsh@gmail.com>
2022-11-02 14:30:20 +0530

    final

---
+++ backend/routes/post.js
@@ -0,0 +1,14 @@
+const express=require("express");
+const { createPost, likeAndUnlikePost, deletePost ,getPostsOfFollowing,updateCaption,commentOnPost,deleteComment}=require("../controllers/post");
+const {isAuthenticated}=require("../middlewares/auth");
+const router=express.Router();
+
+
+router.route("/post/upload").post(isAuthenticated,createPost);
+router.route("/post/:id").get(isAuthenticated,likeAndUnlikePost).delete(isAuthenticated,deletePost);
+
+router.route("/posts").get(isAuthenticated,getPostsOfFollowing);
+router.route("/post/:id").put(isAuthenticated,updateCaption);
+
+router.route("/post/comment/:id").put(isAuthenticated,commentOnPost).delete(isAuthenticated,deleteComment);
+module.exports = router;
---
+++ backend/routes/user.js
@@ -0,0 +1,21 @@
+const express=require("express");
+const {register,login,followUser,logout,updatePassword,updateProfile,deleteMyProfile,myProfile, getAllUser,getUserProfile,forgotPassword,resetPassword, getMyPosts, getUserPosts} =require("../controllers/user");
+const {isAuthenticated}=require("../middlewares/auth");
+
+const router=express.Router();
+
+router.route("/register").post(register);
+router.route("/login").post(login);
+router.route("/follow/:id").get(isAuthenticated,followUser);
+router.route("/logout").get(isAuthenticated,logout);
+router.route("/update/password").put(isAuthenticated,updatePassword);
+router.route("/update/profile").put(isAuthenticated,updateProfile);
+router.route("/delete/me").delete(isAuthenticated,deleteMyProfile);
+router.route("/me").get(isAuthenticated,myProfile);
+router.route("/my/posts").get(isAuthenticated,getMyPosts);
+router.route("/users").get(isAuthenticated,getAllUser);
+router.route("/userposts/:id").get(isAuthenticated, getUserPosts);
+router.route("/users/:id").get(isAuthenticated,getUserProfile);
+router.route("/forgot/password").post(forgotPassword);
+router.route("/password/reset/:token").put(resetPassword);
+module.exports = router;
